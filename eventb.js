5e0f4bbca414338bdda3a960aa44cb4d4707db38
Divjason <sejinpark@tcocs.com>
2025-05-21 22:38:46 +0900

    v1

---
+++ eventb.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("event-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  fetch(
+    "https://script.google.com/macros/s/AKfycbyb8CxzsY3Y6uZgMCGUmrybOo7VXk9Q4behcWsS0th5pC7FRC-U35mqsmOFCU4jQ0j3/exec",
+    {
+      method: "POST",
+      body: formData,
+    }
+  )
+    .then(() => {
+      alert("응모가 완료되었습니다!");
+      form.reset();
+    })
+    .catch(() => alert("제출 오류. 다시 시도해주세요."));
+});
+
+// https://script.google.com/macros/s/AKfycbyb8CxzsY3Y6uZgMCGUmrybOo7VXk9Q4behcWsS0th5pC7FRC-U35mqsmOFCU4jQ0j3/exec
