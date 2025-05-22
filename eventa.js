5f9e6c2687105b2f91ee0f9000075d816166a789
Divjason <sejinpark@tcocs.com>
2025-05-22 21:37:03 +0900

    v3

--- eventa.js
+++ eventa.js
@@ -21,3 +21,13 @@
 // });
 
 // // https://script.google.com/macros/s/AKfycbyb8CxzsY3Y6uZgMCGUmrybOo7VXk9Q4behcWsS0th5pC7FRC-U35mqsmOFCU4jQ0j3/exec
+
+const form = document.getElementById("event-form");
+
+form.addEventListener("submit", function () {
+  // 약간의 지연 후 메인 페이지로 이동
+  setTimeout(() => {
+    alert("응모가 완료되었습니다!");
+    window.location.href = "https://movieevnetpage.netlify.app";
+  }, 1000); // 1초 후 리디렉션 (전송 시간 고려)
+});
